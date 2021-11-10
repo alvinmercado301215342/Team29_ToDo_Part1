94faafaf3f776621c33fb755142b2879176d0536
Alvin Mercado <amercad8@my.centennialcollege.ca>
2021-11-10 17:57:14 -0500

    Added switch

--- Team29_ToDo_Part1/ViewController.swift
+++ Team29_ToDo_Part1/ViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60.0
+    }
+    
+    private let activities = [
+        "Doing homework", "House Cleaning", "Playing games", "Push commits", "Do shopping"
+    ]
+    
+    let simpleTableIdentifier = "SimpleTableIdentifier"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return activities.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        var cell = tableView.dequeueReusableCell(withIdentifier: simpleTableIdentifier)
+        if (cell == nil) {
+            cell = UITableViewCell(
+                style: UITableViewCell.CellStyle.default,
+                reuseIdentifier: simpleTableIdentifier)
+        }
 
-
+        cell?.textLabel?.text = activities[indexPath.row]
+        
+        let switchView = UISwitch(frame: .zero)
+        switchView.setOn(false, animated: true)
+        switchView.tag = indexPath.row
+        switchView.addTarget(self, action: #selector(self.switchChanged(_:)), for: .valueChanged)
+        
+        cell?.accessoryView = switchView
+   
+        return cell!
+    }
+    
+    @objc func switchChanged(_ sender: UISwitch!) {
+        
+        //print("Table row switch Changed \(sender.tag)")
+        //print("The switch is \(sender.isOn ? "ON" : "OFF")")
+        
+    }
 }
 
---
+++ Team29_ToDo_Part1/ViewController2.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController2.swift
+//  Team29_ToDo_Part1
+//
+//  Created by Alvin Carl Mercado on 2021-11-10.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
