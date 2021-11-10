b5482b09d5204897283b1ae45494a553b5de22f5
Alvin Mercado <amercad8@my.centennialcollege.ca>
2021-11-10 16:55:33 -0500

    Initial Commit

---
+++ Team29_ToDo_Part1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Team29_ToDo_Part1
+//
+//  Created by Alvin Carl Mercado on 2021-11-10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
